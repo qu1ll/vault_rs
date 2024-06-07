b0608c67810ea0cb319630c1775b29cdbca9fd4f
qu1ll <elijah.bixby@gmail.com>
2024-06-07 14:48:29 -0400

    Implemented loop in cli Removed unnecessary cli code

--- src/cli.rs
+++ src/cli.rs
@@ -1,27 +1,15 @@
-use std::io;
-
 use clap::{Parser, Subcommand};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
-pub struct Cli {
+pub struct Args {
     #[command(subcommand)]
-    cmd: Commands,
+    pub cmd: Commands,
 }
 
 #[derive(Subcommand, Debug, Clone)]
-pub(crate) enum Commands {
+pub enum Commands {
     Read { file_path: String },
     Write { file_path: String },
-}
-
-impl Cli {
-    pub(crate) fn get_command(&self) -> &Commands {
-        &self.cmd
-    }
-}
-
-pub fn parse_args() -> io::Result<Cli> {
-    Ok(Cli::parse())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::io;
+use clap::{crate_name, Parser};
+use std::io::{self};
 
 mod cli;
 mod crypto;
@@ -6,19 +7,30 @@ mod file_io;
 mod user_input;
 
 use crate::{
-    cli::parse_args,
+    cli::{Args, Commands},
     crypto::{decrypt_data, encrypt_data},
     file_io::{read_file, write_file},
     user_input::read_input,
 };
 
 fn main() -> io::Result<()> {
-    let args = parse_args()?;
+    loop {
+        let mut buf = String::from(crate_name!());
+        buf.push_str(" "); // Add prompt indicator
 
-    match args.get_command() {
-        cli::Commands::Read { file_path } => read_file(file_path.to_string())?,
-        cli::Commands::Write { file_path } => write_file(file_path.to_string())?,
-    };
+        io::stdin().read_line(&mut buf)?; // Use ? for error handling
+        let line = buf.trim();
+        let args = shlex::split(line).ok_or("error: Invalid quoting").unwrap();
 
-    Ok(())
+        match Args::try_parse_from(args.iter()).map_err(|e| e.to_string()) {
+            Ok(cli) => match cli.cmd {
+                Commands::Read { file_path } => read_file(file_path)?,
+                Commands::Write { file_path } => write_file(file_path)?,
+            },
+            Err(e) => println!("Error: {}", e), // Handle parsing error
+        }
+
+        // Add a command to exit the loop (e.g., if line == "quit")
+        // break;
+    }
 }
