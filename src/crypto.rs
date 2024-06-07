842a45a4380bf33f0f9c122d5c7e5a93ee7b1c59
qu1ll <elijah.bixby@gmail.com>
2024-06-06 23:49:02 -0400

    modularized crypto code reduced duplicate code

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,16 +1,17 @@
 use sodiumoxide::crypto::{pwhash, secretbox};
 use std::{
     fs,
-    io::{self, Write},
+    io::{self},
 };
 
-pub(crate) fn encrypt_data(text: String) -> io::Result<Vec<u8>> {
-    let passwd = b"YOUR PASSWORD HERE";
+fn derive_key(passwd: &[u8]) -> io::Result<secretbox::Key>{
 
+    // Read salt to derive key from
     let data = fs::read(".env")?;
     let salt_slice = &data;
     let salt = pwhash::Salt::from_slice(salt_slice).unwrap();
 
+    // Derive key from password and salt
     let mut key = secretbox::Key([0; secretbox::KEYBYTES]);
     {
         let secretbox::Key(ref mut kb) = key;
@@ -24,37 +25,31 @@ pub(crate) fn encrypt_data(text: String) -> io::Result<Vec<u8>> {
         .unwrap();
     }
 
-    // Generate a random nonce (similar to an IV)
-    let nonce = secretbox::gen_nonce();
+    Ok(key) // Return derived key
+}
 
-    let plaintext = text.as_bytes();
-    let ciphertext = secretbox::seal(plaintext, &nonce, &key);
+pub(crate) fn encrypt_data(text: String) -> io::Result<Vec<u8>> {
+    let passwd = b"YOUR PASSWORD HERE"; // Temp password for testing
 
+    let key = derive_key(passwd)?; // Derive key from password
+
+    let nonce = secretbox::gen_nonce(); // Generate a random nonce (similar to an IV)
+
+    let plaintext = text.as_bytes(); // Convert input text into bytes
+    let ciphertext = secretbox::seal(plaintext, &nonce, &key); // Convert input text into ciphertext
+
+    // Combine nonce and ciphertext to be stored
     let mut combined = Vec::with_capacity(secretbox::NONCEBYTES + ciphertext.len());
     combined.extend_from_slice(nonce.as_ref());
     combined.extend_from_slice(&ciphertext);
 
-    Ok(combined)
+    Ok(combined) // Return combined nonce and ciphertext
 }
 
 pub(crate) fn decrypt_data(text: String) -> io::Result<Vec<u8>> {
-    let passwd = b"YOUR PASSWORD HERE";
+    let passwd = b"YOUR PASSWORD HERE"; // Temp password for testing
 
-    let data = fs::read(".env")?;
-    let salt_slice = &data;
-    let salt = pwhash::Salt::from_slice(salt_slice).unwrap();
-    let mut key = secretbox::Key([0; secretbox::KEYBYTES]);
-    {
-        let secretbox::Key(ref mut kb) = key;
-        pwhash::derive_key(
-            kb,
-            passwd,
-            &salt,
-            pwhash::OPSLIMIT_INTERACTIVE,
-            pwhash::MEMLIMIT_INTERACTIVE,
-        )
-        .unwrap();
-    }
+    let key = derive_key(passwd)?; // Derive key from password
 
     let data = fs::read(format!("passwords/{}", text))?; // Path to encrypted data
 
@@ -66,8 +61,5 @@ pub(crate) fn decrypt_data(text: String) -> io::Result<Vec<u8>> {
 
     let decrypted = secretbox::open(&ciphertext, &nonce, &key).unwrap();
 
-    let mut file = fs::File::create("temp.txt")?;
-    file.write_all(&decrypted)?;
-
-    Ok(decrypted)
+    Ok(decrypted) // Return decrypted data
 }
